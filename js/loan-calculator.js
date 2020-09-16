7acec3c879484dfd846744fe2bbe1ab7bb629243
inth3WILD <inth3wild@kali.linux>
2020-09-16 21:54:52 +0100

    Production commit

---
+++ js/loan-calculator.js
@@ -0,0 +1,12 @@
+console.log("Working...")
+
+function calculateResults() {
+    // var P = 30;
+    // var i = 10;
+    // var A = 20;
+    // var N = 5;
+
+    // console.log(P,i,A,N)
+}
+
+calculateResults()

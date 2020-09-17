6344117a2c02668a1e2e8db37350adf2ab8b0e81
inth3WILD <inth3wild@kali.linux>
2020-09-17 21:33:56 +0100

    Added calculation for thr loan

--- js/loan-calculator.js
+++ js/loan-calculator.js
@@ -1,12 +1,24 @@
 console.log("Working...")
 
-function calculateResults() {
+// P = i*A/1-(1+i)^-N
+
+function calculateResults(e) {
     // var P = 30;
     // var i = 10;
     // var A = 20;
     // var N = 5;
 
     // console.log(P,i,A,N)
+
+    var i = document.getElementById("interest").value;
+    var A = document.getElementById("amount").value;
+    var N = document.getElementById("time").value;
+
+    var ans = (i * A) / 1 - Math.pow( (1+i), -N);
+
+    console.log(ans)
+
+    // e.preventDefault();
 }
 
 calculateResults()
